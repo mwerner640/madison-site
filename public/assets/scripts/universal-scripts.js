3d91de04f8e8bb263ffbece826fea993bd446c62
Styx Meiseles <git@davidm.me>
2021-06-08 20:22:54 -0400

    menu icon no jump around

--- public/assets/scripts/universal-scripts.js
+++ public/assets/scripts/universal-scripts.js
@@ -8,11 +8,16 @@
 
     document.querySelector('header.navbar').innerHTML = text;
 
+    // for small screens
+    const navSmallPlaceholderNode = document.querySelector('#nav-small-placeholder');
+    const navSublinksNode = document.querySelector('.nav-sublinks');
+    navSmallPlaceholderNode.appendChild(navSublinksNode.cloneNode(true));
+
     document.querySelector('#menu-icon').addEventListener('click', () => {
 
+        navSmallPlaceholderNode.classList.toggle('hidden');
         document.querySelector('.navbar').classList.toggle('active');
     });
 
 })();
-
 
