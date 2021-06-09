865f7b701f722a884d68c2537e750a49f150ccfb
Styx Meiseles <git@davidm.me>
2021-06-09 14:05:53 -0400

    prioritize loading current image

--- public/assets/scripts/selected-works.js
+++ public/assets/scripts/selected-works.js
@@ -2,24 +2,26 @@
 // used multiple times, so save a completely insignificant amount of time by getting them only once
 const leftArrowContainerNode = document.querySelector('.arrow-left');
 const rightArrowContainerNode = document.querySelector('.arrow-right');
-const imgContainerNode = document.querySelector('#img-container');
-const curImgNode = imgContainerNode.querySelector('#cur-img');
+const curImgNode = document.querySelector('#cur-img');
 const curImgInfoNode = document.querySelector('#cur-img-info');
 
 const slideInfo = {
-    images: [], // note: besides for keys in images.json, there's a dynicamically added key, `cached`
+    images: [],
     curIdx: 0,
     get curImg() {
         return this.images[this.curIdx];
     },
+    predictedNext(from) {
+        return (from + 1 >= this.images.length) ? 0 : from + 1;
+    },
+    predictedPrevious(from) {
+        return (from - 1 < 0) ? this.images.length - 1 : from - 1;
+    },
     next: function() {
-        // tried with modulo operator, but basic math fails me
-        this.curIdx++;
-        this.curIdx = (this.curIdx >= this.images.length) ? 0 : this.curIdx;
+        this.curIdx = this.predictedNext(this.curIdx);
     },
     previous: function() {
-        this.curIdx--;
-        this.curIdx = (this.curIdx < 0) ? this.images.length - 1 : this.curIdx;
+        this.curIdx = this.predictedPrevious(this.curIdx);
     }
 };
 
@@ -29,42 +31,60 @@ const slideInfo = {
     const json = await resp.json();
     slideInfo.images = json.images;
 
+    slideInfo.images.forEach(img => {
+        img.lazyImg = new Image();
+        img.cached = false;
+    });
+
     loadSlide();
-    lazyLoad(1);
+    slideInfo.curImg.lazyImg.onload = () => lazyLoad(1);
+
 })();
 
 /**
  * @param direction - whether to lazy load next images (positive num),
  * or previous images (negative num)
  */
-function lazyLoad(direction = 1) {
+function lazyLoad(direction) {
 
-    const savedIdx = slideInfo.curIdx;
-    const numberOfImagesToPreload = 3; // magic number
+    const numberOfImagesToPreload = 4; // magic number
+    let numberPreloadedSoFar = 0;
 
-    let i = 0;
+    let curLazyLoadIdx = 0;
 
-    while (i < numberOfImagesToPreload) {
-        addLazyImg(slideInfo.curImg);
-        direction > 0 ? slideInfo.next() : slideInfo.previous();
-        i++;
+    let loadNext = () => {
+
+        curLazyLoadIdx = (direction > 0) ?
+            slideInfo.predictedNext(curLazyLoadIdx) :
+            slideInfo.predictedPrevious(curLazyLoadIdx);
+
+        let img = addLazyImg(slideInfo.images[curLazyLoadIdx]);
+        numberPreloadedSoFar++;
+
+        if (numberPreloadedSoFar < numberOfImagesToPreload) {
+            img.addEventListener('load', loadNext);
+        }
     }
 
-    slideInfo.curIdx = savedIdx;
+    loadNext();
 }
+
+/*
+*/
 
 function addLazyImg(imgInfo) {
 
-    if (imgInfo.cached) return;
+    if (imgInfo.cached) return imgInfo.lazyImg;
 
-    const img = new Image();
+    const img = imgInfo.lazyImg;
     img.src = `./assets/images/${imgInfo.src}`;
     imgInfo.cached = true;
     img.loading = 'lazy';
     img.decoding = 'async';
     img.importance = 'low';
     img.classList.add('hidden');
-    imgContainerNode.appendChild(img);
+
+    return imgInfo.lazyImg;
 }
 
 function loadSlide() {
@@ -76,6 +96,9 @@ function loadSlide() {
     const curImgInfo = `<div>${title} (${year})</div><div>${dimensions}</div><div>${medium}</div>`;
     curImgNode.alt = `${title} (${year}) ${dimensions}: ${medium}`;
     curImgInfoNode.innerHTML = curImgInfo;
+
+    slideInfo.curImg.lazyImg = curImgNode;
+    slideInfo.curImg.cached = true;
 }
 
 rightArrowContainerNode.addEventListener('click', () => {
